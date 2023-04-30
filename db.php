cb842d4f8ffe35ffb228100c1486034e44cc8db6
Alice Kuang <66434350+aliceKuang12@users.noreply.github.com>
2023-04-30 15:30:15 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
-	$username = 'a358k336';
-	$password = 'phiaWae4';
+	$username = 'root';
+	$password = '...';
    
 	// Connect to MySQL server, select database
-	$conn = new mysqli('mysql.eecs.ku.edu', $username, $password, $username);
+	$conn = new mysqli(localhost, $username, $password, $username);
 
    // Check connection
    if ($conn->connect_error) {
